b3ebb7e5ddb42f3d5e204819692cfadee2cbd145
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-03-08 22:06:17 -0300

    Adicionar arquivos de projeto.

---
+++ Calcule seu gasto de calorias conforme exercicios/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcule_seu_gasto_de_calorias_conforme_exercicios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
